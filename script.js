85a9bad66c002ce1970e23ba3a48d28a57a6d4b2
vharoc <vharoc@usal.es>
2025-08-23 23:27:15 +0200

    update

--- script.js
+++ script.js
@@ -134,3 +134,93 @@ checkoutForm.addEventListener("submit", async (event) => {
     alert("Hubo un problema al procesar tu pedido. Intenta de nuevo.");
   }
 });
+
+// Provincias y ciudades de ejemplo
+const data = {
+    "Álava": ["Alegría-Dulantzi","Amurrio","Añana","Aramaio","Armiñón","Arraia-Maeztu","Arrazua-Ubarrundia","Artziniega","Asparrena","Ayala (Aiara)","Baños de Ebro (Mañueta)","Barrundia","Berantevilla","Bernedo","Campezo (Kanpezu)","Cripán","Elburgo (Burgelu)","Elciego","Elvillar (Bilar)","Harana (Valle de Arana)","Iruña de Oca (Iruña Oka)","Iruraiz-Gauna","Kuartango","Labastida","Lagrán","Laguardia","Lanciego (Lantziego)","Lantarón","Lapuebla de Labarca","Laudio (Llodio)","Legutio","Leza","Moreda de Álava","Navaridas","Okondo","Oyón-Oion","Peñacerrada-Urizaharra","Ribera Alta","Ribera Baja (Erribera Beitia)","Salvatierra (Agurain)","Samaniego","San Millán (Donemiliaga)","Urkabustaiz","Valdegovía","Villabuena de Álava (Eskuernaga)","Vitoria-Gasteiz","Yécora (Iekora)","Zalduondo","Zambrana","Zigoitia","Zuia"],
+    "Albacete": ["Abengibre", "Alatoz", "Albacete", "Albatana", "Alborea", "Alcadozo", "Alcalá del Júcar", "Alcaraz", "Almansa", "Alpera", "Ayna", "Balazote", "Balsa de Ves", "Barrax", "Bienservida", "Bogarra", "Bonete", "Carcelén", "Casas de Juan Núñez", "Casas de Lázaro", "Casas de Ves", "Casas-Ibáñez", "Caudete", "Cenizate", "Chinchilla de Monte-Aragón", "Corral-Rubio", "El Bonillo", "Elche de la Sierra", "Férez", "Fuente-Álamo", "Fuentealbilla", "Golosalvo", "Hellín", "Hoya-Gonzalo", "Jorquera", "La Gineta", "La Herrera", "La Rasa", "La Recueja", "La Toba", "Letur", "Liétor", "Madrigueras", "Masegoso", "Minaya", "Molinicos", "Montalvos", "Montealegre del Castillo", "Munera", "Nerpio", "Navas de Jorquera", "Noguera", "Ontur", "Ossa de Montiel", "Paterna del Madera", "Peñas de San Pedro", "Povedilla", "Pozo Cañada", "Pozohondo", "Riópar", "Robledo", "Salobre", "San Pedro", "San Sebastián de los Reyes", "Santa Ana", "Santa Cruz de Moya", "Santa María del Campo", "Santa Olalla", "Serranía de Cuenca", "Siles", "Socovos", "Tarazona de la Mancha", "Tobarra", "Valdeganga", "Villarrobledo", "Villavaliente", "Villanueva de Alcardete", "Villanueva de Andújar", "Villanueva de la Jara", "Villanueva de los Infantes", "Villanueva de los Caballeros", "Villanueva de los Llanos", "Villanueva de los Pinares", "Villanueva del Arzobispo", "Villanueva del Duque", "Villanueva del Río", "Villanueva del Valle", "Villanueva del Yeltes", "Villanueva de la Fuente", "Villanueva de la Vera", "Villanueva de los Remedios", "Villanueva de los Santos", "Villanueva de los Tilos", "Villanueva de los Valles", "Villanueva del Álamo", "Villanueva del Campo", "Villanueva del Duero", "Villanueva del Río y Minas", "Villanueva del Yelmo"],
+    "Alicante": ["Alicante", "Elche", "Orihuela", "Benidorm", "Torrevieja"],
+    "Almería": ["Almería", "Roquetas de Mar", "El Ejido", "Níjar"],
+    "Asturias": ["Oviedo", "Gijón", "Avilés", "Mieres"],
+    "Ávila": ["Ávila", "Arévalo", "Arenas de San Pedro"],
+    "Badajoz": ["Badajoz", "Mérida", "Don Benito", "Almendralejo"],
+    "Barcelona": ["Barcelona", "Hospitalet", "Badalona", "Sabadell", "Terrassa"],
+    "Burgos": ["Burgos", "Miranda de Ebro", "Aranda de Duero"],
+    "Cáceres": ["Cáceres", "Plasencia", "Navalmoral de la Mata"],
+    "Cádiz": ["Cádiz", "Jerez de la Frontera", "Algeciras", "San Fernando", "El Puerto de Santa María"],
+    "Cantabria": ["Santander", "Torrelavega", "Castro-Urdiales", "Camargo"],
+    "Castellón": ["Castellón de la Plana", "Villarreal", "Burriana", "La Vall d'Uixó"],
+    "Ceuta": ["Ceuta"],
+    "Ciudad Real": ["Ciudad Real", "Puertollano", "Tomelloso", "Valdepeñas"],
+    "Córdoba": ["Córdoba", "Lucena", "Puente Genil", "Montilla"],
+    "Cuenca": ["Cuenca", "Tarancón", "San Clemente"],
+    "Girona": ["Girona", "Figueres", "Blanes", "Lloret de Mar"],
+    "Granada": ["Granada", "Motril", "Almuñécar", "Baza"],
+    "Guadalajara": ["Guadalajara", "Azuqueca de Henares", "Alovera"],
+    "Gipuzkoa": ["San Sebastián", "Irun", "Eibar", "Rentería"],
+    "Huelva": ["Huelva", "Lepe", "Isla Cristina", "Ayamonte"],
+    "Huesca": ["Huesca", "Monzón", "Barbastro", "Jaca"],
+    "Illes Balears": ["Palma de Mallorca", "Ibiza", "Manacor", "Inca"],
+    "Jaén": ["Jaén", "Linares", "Andújar", "Úbeda"],
+    "La Coruña": ["A Coruña", "Santiago de Compostela", "Ferrol", "Oleiros"],
+    "La Rioja": ["Logroño", "Calahorra", "Haro", "Arnedo"],
+    "Las Palmas": ["Las Palmas de Gran Canaria", "Telde", "Santa Lucía de Tirajana", "Arrecife"],
+    "León": ["León", "Ponferrada", "San Andrés del Rabanedo", "Astorga"],
+    "Lleida": ["Lleida", "Balaguer", "Tàrrega", "La Seu d'Urgell"],
+    "Lugo": ["Lugo", "Monforte de Lemos", "Viveiro"],
+    "Madrid": ["Madrid", "Alcalá de Henares", "Getafe", "Leganés", "Móstoles"],
+    "Málaga": ["Málaga", "Marbella", "Fuengirola", "Torremolinos", "Estepona"],
+    "Melilla": ["Melilla"],
+    "Murcia": ["Murcia", "Cartagena", "Lorca", "Molina de Segura"],
+    "Navarra": ["Pamplona", "Tudela", "Barañáin", "Estella"],
+    "Ourense": ["Ourense", "Verín", "O Barco de Valdeorras"],
+    "Palencia": ["Palencia", "Aguilar de Campoo", "Guardo"],
+    "Pontevedra": ["Vigo", "Pontevedra", "Vilagarcía de Arousa", "Marín"],
+    "Salamanca": ["Salamanca", "Béjar", "Ciudad Rodrigo"],
+    "Santa Cruz de Tenerife": ["Santa Cruz de Tenerife", "San Cristóbal de La Laguna", "Arona", "La Orotava"],
+    "Segovia": ["Segovia", "Cuéllar", "El Espinar"],
+    "Sevilla": ["Sevilla", "Dos Hermanas", "Alcalá de Guadaíra", "Utrera"],
+    "Soria": ["Soria", "Almazán", "Ólvega"],
+    "Tarragona": ["Tarragona", "Reus", "Valls", "Cambrils"],
+    "Teruel": ["Teruel", "Alcañiz", "Andorra"],
+    "Toledo": ["Toledo", "Talavera de la Reina", "Illescas"],
+    "Valencia": ["Valencia", "Gandía", "Torrent", "Paterna", "Sagunto"],
+    "Valladolid": ["Valladolid", "Medina del Campo", "Laguna de Duero"],
+    "Zamora": ["Zamora", "Benavente", "Toro"],
+    "Zaragoza": ["Zaragoza", "Calatayud", "Utebo", "Ejea de los Caballeros"]
+};
+
+const provinciaSelect = document.getElementById("provincia");
+const ciudadSelect = document.getElementById("ciudad");
+
+// Cargar provincias al iniciar
+function cargarProvincias() {
+  Object.keys(data).forEach(prov => {
+    const option = document.createElement("option");
+    option.value = prov;
+    option.textContent = prov;
+    provinciaSelect.appendChild(option);
+  });
+
+}
+
+// Cargar ciudades cuando se elige provincia
+provinciaSelect.addEventListener("change", () => {
+  const provincia = provinciaSelect.value;
+  ciudadSelect.innerHTML = "<option value=''>-- Selecciona una ciudad --</option>";
+
+  if (provincia && data[provincia]) {
+    ciudadSelect.disabled = false;
+    data[provincia].forEach(ciudad => {
+      const option = document.createElement("option");
+      option.value = ciudad;
+      option.textContent = ciudad;
+      ciudadSelect.appendChild(option);
+    });
+  } else {
+    ciudadSelect.disabled = true;
+  }
+});
+
+// Inicializar
+cargarProvincias();
